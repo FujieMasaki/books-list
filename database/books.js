b11663ac14dde38d643aac98f8bc473afe301af8
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-12-06 16:20:46 +0900

    ✨ 読書ずみのフラグを追加

--- database/books.js
+++ database/books.js
@@ -4,17 +4,20 @@ exports.books = [
     title: "オズの魔法使",
     author: "ライマン・フランク・ボーム",
     categoryId: "literature",
+    isRead: true,
   },
   {
     id: 2,
     title: "風と共に去りぬ",
     author: "マーガレット・ミッチェル",
     categoryId: "literature",
+    isRead: false,
   },
   {
     id: 3,
     title: "人を動かす",
     author: "D・カーネギー",
     categoryId: "self-help",
+    isRead: true,
   },
 ];
