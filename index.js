c960cae93591c0f952ded882cf1bbbeee95c3eee
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-12-06 11:28:34 +0900

    ✨ BookとCategoryを関連づける

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { args } = require("commander");
 
 const books = [
   {
     id: 1,
     title: "オズの魔法使",
     author: "ライマン・フランク・ボーム",
+    categoryId: "literature",
   },
   {
     id: 2,
     title: "風と共に去りぬ",
     author: "マーガレット・ミッチェル",
+    categoryId: "literature",
+  },
+  {
+    id: 3,
+    title: "人を動かす",
+    author: "D・カーネギー",
+    categoryId: "self-help",
+  },
+];
+
+const categories = [
+  {
+    id: "literature",
+    name: "文学",
+  },
+  {
+    id: "self-help",
+    name: "自己啓発",
   },
 ];
 
@@ -17,12 +37,21 @@ const typeDefs = gql`
   type Query {
     books: [Book!]!
     book(id: Int!): Book
+    categories: [Category!]!
+    category(id: ID!): Category
   }
 
   type Book {
     id: Int!
     title: String!
     author: String!
+    category: Category!
+  }
+
+  type Category {
+    id: ID!
+    name: String!
+    books: [Book!]!
   }
 `;
 
@@ -34,6 +63,27 @@ const resolvers = {
       const book = books.find((book) => book.id === bookId);
       if (!book) return null;
       return book;
+    },
+    categories: () => categories,
+    category: (parent, args, context) => {
+      const categoryId = args.id;
+      const category = categories.find(
+        (category) => category.id === categoryId
+      );
+      if (!category) return null;
+      return category;
+    },
+  },
+  Category: {
+    books: (parent, args, context) => {
+      const id = parent.id;
+      return books.filter((book) => book.categoryId === id);
+    },
+  },
+  Book: {
+    category: (parent, args, context) => {
+      const categoryId = parent.categoryId;
+      return categories.find((category) => category === category);
     },
   },
 };
