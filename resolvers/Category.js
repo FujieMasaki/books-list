196c395d9126733f1adb4c136e691a6f9a9cdca4
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-12-06 18:34:29 +0900

    ✨ mutationを使用・データをdbにまとめた

--- resolvers/Category.js
+++ resolvers/Category.js
@@ -1,7 +1,7 @@
 const { categories } = require("../database/categories");
 
 exports.Category = {
-  books: ({ id }, args, { books }) => {
+  books: ({ id }, args, { db }) => {
     return books.filter((book) => book.categoryId === id);
   },
 };
